087229db22ea19d8b2a3562d0b0d504c2936b57a
NGK Sternhagen <nicholas.sternhagen@babycenter.com>
2016-10-10 16:46:08 -0700

    initial

---
+++ cwd-follows-focus.el
@@ -0,0 +1,13 @@
+;;; cwd-follows-focus.el --- TODO summary
+
+;; Copyright (C) 2016 NGK Sternhagen
+
+;; Author: NGK Sternhagen <sternhagen@protonmail.ch>
+;; Version: 0.0.0
+;; URL: https://gitlab.com/skwuent/cwd-follows-focus
+
+;;; Commentary:
+
+
+;;;###autoload
+(f-touch "/tmp/cwd-file")
